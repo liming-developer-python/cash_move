4d0cf869a2d2f40c7ca49d26d61b91b57e4bf6ce
Lee Ming <75870530+liming-developer-python@users.noreply.github.com>
2021-06-29 22:00:52 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,97 +26,97 @@
     <!-- Hero Area End -->
 
     <!-- Services Section Start -->
-    <section id="services" class="section-padding">
-        <div class="container">
-            <div class="section-header text-center">
-                <h2 class="section-title wow fadeInDown" data-wow-delay="0.3s">Options</h2>
-                <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div>
-            </div>
-            <div class="row">
-                <!-- Services item -->
-                <div class="col-md-6 col-lg-4 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.3s">
-                        <div class="icon">
-                            <i class="lni-cog"></i>
-                        </div>
-                        <div class="services-content">
-                            <h3><a>My Profile</a></h3>
-                            <p style="font-size: 0.8rem; color: black;">You can check your profile. </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Services item -->
-                <div class="col-md-6 col-lg-4 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
-                        <div class="icon">
-                            <i class="lni-stats-up"></i>
-                        </div>
-                        <div class="services-content">
-                            <h3><a>My account</a></h3>
-                            <p style="font-size: 0.8rem; color: black;">You can check your account. </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Services item -->
-                <div class="col-md-6 col-lg-4 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.9s">
-                        <div class="icon">
-                            <i class="lni-users"></i>
-                        </div>
-                        <div class="services-content">
-                            <h3><a>My Group</a></h3>
-                            <p style="font-size: 0.8rem; color: black;">You can get money in group. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+{{--    <section id="services" class="section-padding">--}}
+{{--        <div class="container">--}}
+{{--            <div class="section-header text-center">--}}
+{{--                <h2 class="section-title wow fadeInDown" data-wow-delay="0.3s">Options</h2>--}}
+{{--                <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div>--}}
+{{--            </div>--}}
+{{--            <div class="row">--}}
+{{--                <!-- Services item -->--}}
+{{--                <div class="col-md-6 col-lg-4 col-xs-12">--}}
+{{--                    <div class="services-item wow fadeInRight" data-wow-delay="0.3s">--}}
+{{--                        <div class="icon">--}}
+{{--                            <i class="lni-cog"></i>--}}
+{{--                        </div>--}}
+{{--                        <div class="services-content">--}}
+{{--                            <h3><a>My Profile</a></h3>--}}
+{{--                            <p style="font-size: 0.8rem; color: black;">You can check your profile. </p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- Services item -->--}}
+{{--                <div class="col-md-6 col-lg-4 col-xs-12">--}}
+{{--                    <div class="services-item wow fadeInRight" data-wow-delay="0.6s">--}}
+{{--                        <div class="icon">--}}
+{{--                            <i class="lni-stats-up"></i>--}}
+{{--                        </div>--}}
+{{--                        <div class="services-content">--}}
+{{--                            <h3><a>My account</a></h3>--}}
+{{--                            <p style="font-size: 0.8rem; color: black;">You can check your account. </p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- Services item -->--}}
+{{--                <div class="col-md-6 col-lg-4 col-xs-12">--}}
+{{--                    <div class="services-item wow fadeInRight" data-wow-delay="0.9s">--}}
+{{--                        <div class="icon">--}}
+{{--                            <i class="lni-users"></i>--}}
+{{--                        </div>--}}
+{{--                        <div class="services-content">--}}
+{{--                            <h3><a>My Group</a></h3>--}}
+{{--                            <p style="font-size: 0.8rem; color: black;">You can get money in group. </p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </section>--}}
     <!-- Services Section End -->
 
     <!-- About Section start -->
-    <div class="about-area section-padding bg-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-2 col-xs-2"></div>
-                <div class="col-lg-6 col-md-8 col-xs-8 info" style="text-align: center;">
-                    <div class="about-wrapper wow fadeInLeft" data-wow-delay="0.3s">
-                        <div>
-                            <div class="site-heading">
-                                <h2 class="section-title">here is a description what you want</h2>
-                            </div>
-                            <div class="content">
-                                <p style="font-size: 1rem; color: black;">
-                                    here is a description what you want
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6 col-md-12 col-xs-12 info">
-                    <div class="about-wrapper wow fadeInLeft" data-wow-delay="0.3s">
-                        <div>
-                            <div class="site-heading">
+{{--    <div class="about-area section-padding bg-gray">--}}
+{{--        <div class="container">--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-lg-3 col-md-2 col-xs-2"></div>--}}
+{{--                <div class="col-lg-6 col-md-8 col-xs-8 info" style="text-align: center;">--}}
+{{--                    <div class="about-wrapper wow fadeInLeft" data-wow-delay="0.3s">--}}
+{{--                        <div>--}}
+{{--                            <div class="site-heading">--}}
+{{--                                <h2 class="section-title">here is a description what you want</h2>--}}
+{{--                            </div>--}}
+{{--                            <div class="content">--}}
+{{--                                <p style="font-size: 1rem; color: black;">--}}
+{{--                                    here is a description what you want--}}
+{{--                                </p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-lg-6 col-md-12 col-xs-12 info">--}}
+{{--                    <div class="about-wrapper wow fadeInLeft" data-wow-delay="0.3s">--}}
+{{--                        <div>--}}
+{{--                            <div class="site-heading">--}}
 {{--                                <p class="mb-3">Manage Statistics</p>--}}
-                                <h2 class="section-title">here is a description what you want</h2>
-                            </div>
-                            <div class="content">
-                                <p style="font-size: 1rem; color: black;">
-                                    here is a description what you want
-                                </p>
-                                <a href="#" class="btn btn-common mt-3" style="font-size: 0.9rem; color: black;">here is a description what you want</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-12 col-xs-12 wow fadeInRight" data-wow-delay="0.3s">
-                    <img class="img-fluid" src="{{ asset('img/about/img-1.png') }}" alt="" >
-                </div>
-            </div>
-        </div>
-    </div>
+{{--                                <h2 class="section-title">here is a description what you want</h2>--}}
+{{--                            </div>--}}
+{{--                            <div class="content">--}}
+{{--                                <p style="font-size: 1rem; color: black;">--}}
+{{--                                    here is a description what you want--}}
+{{--                                </p>--}}
+{{--                                <a href="#" class="btn btn-common mt-3" style="font-size: 0.9rem; color: black;">here is a description what you want</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-6 col-md-12 col-xs-12 wow fadeInRight" data-wow-delay="0.3s">--}}
+{{--                    <img class="img-fluid" src="{{ asset('img/about/img-1.png') }}" alt="" >--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <!-- About Section End -->
 
 @endsection
